04ce5f5666bc4f558b9b513991196195376dfe23
Navneet Melarkode <navneet.melarkode@gmail.com>
2020-08-30 12:32:51 +0530

    DA2

--- concurrentPractice.py
+++ concurrentPractice.py
@@ -1,39 +1,49 @@
 from concurrent.futures import ThreadPoolExecutor
 
-from bs4 import BeautifulSoup
 import requests
+from bs4 import BeautifulSoup
 
-class concurrentCrawler(object):
+class ConcurrentListCrawler(object):
+
     def __init__(self, url_list, threads):
+
         self.urls = url_list
         self.results = {}
         self.max_threads = threads
-    def __make_request(self,url):
+
+    def __make_request(self, url):
         try:
-            r = requests.get(url, time=20)
+            r = requests.get(url=url, timeout=20)
             r.raise_for_status()
         except requests.exceptions.Timeout:
             r = requests.get(url=url, timeout=60)
         except requests.exceptions.ConnectionError:
-            r = requests.get(url=url, timeout = 60)
+            r = requests.get(url=url, timeout=60)
         except requests.exceptions.RequestException as e:
             raise e
         return r.url, r.text
+
     def __parse_results(self, url, html):
+
         try:
             soup = BeautifulSoup(html, 'html.parser')
             title = soup.find('title').get_text()
         except Exception as e:
             raise e
+
         if title:
             self.results[url] = title
+
     def wrapper(self, url):
         url, html = self.__make_request(url)
         self.__parse_results(url, html)
+
     def run_script(self):
         with ThreadPoolExecutor(max_workers=min(len(self.urls),self.max_threads)) as Executor:
             jobs = [Executor.submit(self.wrapper, u) for u in self.urls]
-if __name__=='__main__':
+
+
+if __name__ == '__main__':
     example = ConcurrentListCrawler(['https://edmundmartin.com',
                                      'https://www.udemy.com',
                                      'https://github.com/',
